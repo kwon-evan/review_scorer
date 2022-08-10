5399c2efdbe2e1f5c9847881015fd08aa28d768e
heonjin <koun012@naver.com>
2022-08-10 14:36:57 +0900

    v1.0.0

--- review_scorer/review_scorer.py
+++ review_scorer/review_scorer.py
@@ -24,15 +24,18 @@ class ReviewScorer(Word2Vec):
             trim_rule=None, sorted_vocab=1, batch_words=MAX_WORDS_IN_BATCH, compute_loss=False, callbacks=(),
             comment=None, max_final_vocab=None, shrink_windows=True,
     ):
-        super().__init__(sentences=sentences, corpus_file=corpus_file, vector_size=vector_size, alpha=alpha,
-                         window=window, min_count=min_count, max_vocab_size=max_vocab_size, sample=sample, seed=seed,
-                         workers=workers, min_alpha=min_alpha, sg=sg, hs=hs, negative=negative, ns_exponent=ns_exponent,
-                         cbow_mean=cbow_mean, hashfxn=hashfxn, epochs=epochs, null_word=null_word, trim_rule=trim_rule,
-                         sorted_vocab=sorted_vocab, batch_words=batch_words, compute_loss=compute_loss,
-                         callbacks=callbacks, comment=comment, max_final_vocab=max_final_vocab,
-                         shrink_windows=shrink_windows, )
+        super().__init__(
+            sentences=sentences, corpus_file=corpus_file, vector_size=vector_size, alpha=alpha,
+            window=window, min_count=min_count, max_vocab_size=max_vocab_size, sample=sample, seed=seed,
+            workers=workers, min_alpha=min_alpha, sg=sg, hs=hs, negative=negative, ns_exponent=ns_exponent,
+            cbow_mean=cbow_mean, hashfxn=hashfxn, epochs=epochs, null_word=null_word, trim_rule=trim_rule,
+            sorted_vocab=sorted_vocab, batch_words=batch_words, compute_loss=compute_loss,
+            callbacks=callbacks, comment=comment, max_final_vocab=max_final_vocab,
+            shrink_windows=shrink_windows
+        )
         if not hasattr(self, 'wv'):  # set unless subclass already set (eg: FastText)
             self.wv = KeyedVectors(vector_size)
+
         self.category_list = None
         self.is_tagged = False
         with open(file=senti_dict_path,
@@ -48,11 +51,11 @@ class ReviewScorer(Word2Vec):
         return result
 
     def tag(self, categories: dict, topn: int = 100):
-        self.category_list = categories.keys()
         for category, words in categories.items():
-            logger.info(f'tagging {category}')
+            logger.info(f'category \"{category}\" tagged')
             self.senti[category] = False
             self.senti.loc[self.get_similar_words_indexes(words=words, topn=topn), category] = True
+        self.category_list = categories.keys()
         self.is_tagged = True
 
     def score_review(self,
@@ -62,7 +65,7 @@ class ReviewScorer(Word2Vec):
         """
         if self.is_tagged is not True:
             raise Exception('Sentimental Dictionary of Review Scorer is not tagged yet. '
-                            'Use method ReviewScorer.tag.')
+                            'Use method ReviewScorer.tag().')
 
         _scored = self.senti.loc[self.senti.word.isin(tokenized_review)].copy()
 
